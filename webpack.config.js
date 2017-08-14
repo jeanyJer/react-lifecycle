62529a9ed2f5f93d485f6965673c793276573bc6
devxu <1181872953@qq.com>
2017-08-14 15:06:50 +0800

    add webpack package index files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: '.index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{test: /\.css$/, loader: 'style-loader!css-loader'},
+			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({template: './index.html'})
+	]
+}
